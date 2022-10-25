cf20438b094b94a4f67b5a982bafd2120d952a8b
broswilli <broswilli@gmail.com>
2022-10-25 22:33:59 +0100

    Completed chapter 17

---
+++ vue-loader/lesson/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>The count is {{ count }}</h1>
+    <p>
+      <button @click="count += 1">Count</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return { count: 0 }
+    }
+  }
+</script>
